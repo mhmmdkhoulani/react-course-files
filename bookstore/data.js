80359297be3f530ffacd7bc78e5304fd8d4622d3
Mohammad <mhmmdkholani@gmail.com>
2024-04-24 11:20:08 +0300

    Add books data as array of objects

---
+++ bookstore/data.js
@@ -0,0 +1,30 @@
+const books = [
+  {
+    bookName: "After You",
+    author: "Jojo Myoes",
+    rating: 1.9,
+    info: "It continues the story of Louisa Clark after Will's death. She is trying to move on.",
+    cover: "images/afteryou.jpg",
+  },
+  {
+    bookName: "Big Magi",
+    author: "Elizabeth Gilbert",
+    rating: 1.2,
+    info: "Readers of all ages and walks of life have drawn inspiration from Elizabeth Gilbert’s books.",
+    cover: "images/bigmagic.jpg",
+  },
+  {
+    bookName: "A Tale for the Time Being",
+    author: "Ruth Ozeki",
+    rating: 1.9,
+    info: "In Tokyo, sixteen-year-old Nao has decided there’s only one escape from her aching loneliness",
+    cover: "images/tale.jpg",
+  },
+  {
+    bookName: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    rating: 1.9,
+    info: "The novel was inspired by a youthful romance Fitzgerald had with socialite Ginevra King",
+    cover: "images/greatgatsby.jpg",
+  },
+];
